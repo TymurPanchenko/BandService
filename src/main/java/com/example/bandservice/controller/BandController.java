e2111a901384648abd49ccb8ba13d7ff016f7d28
Timur <timurkekwtf@gmail.com>
2022-02-23 22:18:01 +0200

    logging

--- src/main/java/com/example/bandservice/controller/BandController.java
+++ src/main/java/com/example/bandservice/controller/BandController.java
@@ -3,6 +3,8 @@ package com.example.bandservice.controller;
 import com.example.bandservice.exception.NullBandReferenceException;
 import com.example.bandservice.model.Band;
 import com.example.bandservice.service.BandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.Objects;
 @RequestMapping("/bands")
 public class BandController {
     private final BandService bandService;
+    private final Logger logger = LoggerFactory.getLogger(BandController.class);
 
     public BandController(BandService bandService) {
         this.bandService = bandService;
@@ -21,6 +24,7 @@ public class BandController {
 
     @PostMapping
     public ResponseEntity<Band> saveBand(@Valid @RequestBody Band band, Errors errors) {
+        logger.info("Creating new band");
         if (errors.hasErrors()) {
             throw new NullBandReferenceException("Band is not valid");
         }
@@ -33,11 +37,13 @@ public class BandController {
 
     @GetMapping
     public ResponseEntity<Band> getBand(@RequestParam("bandName") String name) {
+        logger.info("Getting band name = {}", name);
         return ResponseEntity.ok(bandService.readByName(name));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Band> findBandById(@PathVariable("id") Long id) {
+        logger.info("Getting band id = {}", id);
         Band band = bandService.readById(id);
         if (Objects.isNull(band)) {
             throw new NullBandReferenceException("Not found");
@@ -48,11 +54,13 @@ public class BandController {
 
     @DeleteMapping("/{id}")
     public void deleteBand(@PathVariable("id") Long id) {
+        logger.info("Deleting band id = {}", id);
         bandService.delete(id);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Band> updateBand(@PathVariable("id") Long id, @RequestBody Band band) {
+        logger.info("Updating band id = {}", id);
         return ResponseEntity.ok(bandService.update(id, band));
     }
 }
